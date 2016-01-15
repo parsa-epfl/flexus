632b289d26c3ca0867a066bf0e6c59977d92ebd6
Mario Drumond <mario.drumond@epfl.ch>
2016-01-15 17:17:52 +0100

    fixing makefile scripts

--- config_env.sh
+++ config_env.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-TOOLCHAIN_BASE=/mnt/hgfs/scratch/deps/toolchain-5.2
-export PATH=$TOOLCHAIN_BASE/bin:$PATH
-export LD_LIBRARY_PATH=$TOOLCHAIN_BASE/lib:$TOOLCHAIN_BASE/lib64:/usr/lib32:$LD_LIBRARY_PATH
+TOOLCHAIN_BASE=/mnt/hgfs/scratch/deps/
+export LD_LIBRARY_PATH=$TOOLCHAIN_BASE/libs/lib:$LD_LIBRARY_PATH
