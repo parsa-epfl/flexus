9b44b0cef283bec83d2a538d5541bda813f2919c
Mark Sutherland <mark.sutherland@epfl.ch>
2019-06-12 15:01:31 +0200

    Fixes Tmp debug level, previously it was equal to VVerb.

--- core/debug/aux_/tmp_macros.hpp
+++ core/debug/aux_/tmp_macros.hpp
@@ -1,14 +1,14 @@
-#if BOOST_PP_LESS_EQUAL( DBG__internal_tmp_MinimumSeverity, DBG_internal_Sev_to_int(VVerb) )
-//Crit debugging enabled
-#if ( DBG__internal_tmp_MinimumSeverity == DBG_internal_Sev_to_int(VVerb) )
-#define DBG__internal_MinimumSeverity DBG_internal_Sev_to_int(VVerb)
+#if BOOST_PP_LESS_EQUAL( DBG__internal_tmp_MinimumSeverity, DBG_internal_Sev_to_int(Tmp) )
+//Tmp debugging enabled
+#if ( DBG__internal_tmp_MinimumSeverity == DBG_internal_Sev_to_int(Tmp) )
+#define DBG__internal_MinimumSeverity DBG_internal_Sev_to_int(Tmp)
 #endif
 
 #define DBG__internal_Tmp( Sev, operations )        \
     DBG__internal_PROCESS_DBG( Sev, operations )      /**/
 
 #else
-//Crit debugging disabled
+//Tmp debugging disabled
 
 #define DBG__internal_Tmp( ... ) do {} while(0)
 
