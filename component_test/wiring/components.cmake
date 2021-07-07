d4118e0d4352a56592ea5aa6c07bfb5f74097ef7
Setu Gupta <setu18190@iiitd.ac.in>
2021-07-07 10:47:51 +0200

    Removed unnecessary options from components.cmake
    On branch testing_harness
    Changes to be committed:
    	modified:   wiring/components.cmake

--- component_test/wiring/components.cmake
+++ component_test/wiring/components.cmake
@@ -3,15 +3,3 @@ set(REQUIRED_COMPONENTS
   MemoryLoopback 
   MemoryMap
 )
-#MemoryMap 
-#  CMPCache 
-#  MMU
-#)
-
-set(SUPPORTS_SIMICS false)
-set(SUPPORTS_QEMU true)
-set(DEFAULT_TARGET qemu)
-set(SUPPORTS_X86 false)
-set(SUPPORTS_V9 false)
-set(SUPPORTS_ARM true)
-set(DEFAULT_PLATFORM arm)
