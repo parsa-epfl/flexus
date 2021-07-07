687faed3b660421ebbd717b659e76d317eca6690
Mark Sutherland <mark.sutherland@epfl.ch>
2021-07-07 10:06:28 +0200

    Create GH action pipelines for build testing (#33)
    * Tests building flexus (both trace mode and timing mode).
    * Checks for clang-format and automatically fixes the issues.

--- .github/scripts/build_frozen_pkgs.sh
+++ .github/scripts/build_frozen_pkgs.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #  DO-NOT-REMOVE begin-copyright-block
 # QFlex consists of several software components that are governed by various
 # licensing terms, in addition to software that was developed internally.
@@ -15,7 +17,7 @@
 # **QFlex License**
 #
 # QFlex
-# Copyright (c) 2020, Parallel Systems Architecture Lab, EPFL
+# Copyright (c) 2021, Parallel Systems Architecture Lab, EPFL
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
@@ -43,35 +45,40 @@
 # THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #  DO-NOT-REMOVE end-copyright-block
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
-dist: bionic
-language: cpp
+set -x
+set -e
 
-addons:
-  apt:
-    packages:
-    - clang-format-6.0
-    - cmake
-    - libboost-all-dev  
+JOBS=$(($(getconf _NPROCESSORS_ONLN) + 1))
+echo "=== Using ${JOBS} simultaneous jobs ==="
 
-matrix:
-  include:
-    - env:
-      - SIMULATOR="KeenKraken"
-    - env:
-      - SIMULATOR="KnottyKraken"
-    - env:
-      - SIMULATOR="KeenKraken"
-      - $BUILD_BOOST=true
-    - env:
-      - SIMULATOR="KnottyKraken"
-      - $BUILD_BOOST=true
-    - env:
-      - LINT=true
+# Install a compatible version of gcc
+GCC_VERSION="8"
+sudo apt-get update -qq
+sudo apt-get autoremove libboost-all-dev
+sudo apt-get -y install gcc-${GCC_VERSION} g++-${GCC_VERSION}
 
-script:
-  - git clone https://github.com/parsa-epfl/libqflex.git ../libqflex
-  - if [ "$LINT" = true ];
-    then ${TRAVIS_BUILD_DIR}/tests/clang_format_test.sh;
-    else ${TRAVIS_BUILD_DIR}/tests/build_flexus.sh ${SIMULATOR};
-    fi
+# Set the recently installed version of gcc as default
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${GCC_VERSION} 20
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-${GCC_VERSION} 20
+sudo update-alternatives --config gcc
+sudo update-alternatives --config g++
+
+# Install a compatible version of boost library
+BOOST="boost_1_70_0"
+BOOST_VERSION="1.70.0"
+BOOST_SHA="882b48708d211a5f48e60b0124cf5863c1534cd544ecd0664bb534a4b5d506e9"
+wget https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/source/${BOOST}.tar.gz -O /tmp/${BOOST}.tar.gz
+echo "${BOOST_SHA} /tmp/${BOOST}.tar.gz | sha256sum --check --status"
+if [ $? -ne 0 ]; then
+  echo "SHA256 for boost tarball failed!"
+  exit 1
+fi
+tar -xf /tmp/${BOOST}.tar.gz
+cd ./${BOOST}/
+./bootstrap.sh --prefix=/usr/local
+./b2 -j${JOBS} --with-system --with-regex --with-serialization --with-iostreams
+sudo ./b2 --with-system --with-regex --with-serialization --with-iostreams install
