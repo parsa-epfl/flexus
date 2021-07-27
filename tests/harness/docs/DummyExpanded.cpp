4b6373aac19c242dd33acdf57c58bffc3ba78e50
Setu-Gupta <setu18190@iiitd.ac.in>
2021-07-27 20:09:59 +0530

    Updated DymmyExpanded to incorporate dynamic ports
    On branch testing_harness
    Changes to be committed:
    	modified:   .gitignore
    	modified:   tests/harness/docs/DummyExpanded.cpp

--- tests/harness/docs/DummyExpanded.cpp
+++ tests/harness/docs/DummyExpanded.cpp
@@ -110,6 +110,42 @@ struct DummyInterface : public Flexus::Core::ComponentInterface
     virtual pullStateRet::payload pull(pullStateRet const &) = 0;
     virtual bool available(pullStateRet const &) = 0;
 
+    struct setStateDyn
+    {
+        typedef int payload;
+        typedef Flexus::Core::aux_::push port_type;
+        static const bool is_array = true;
+    };
+    virtual void push(setStateDyn const &, Flexus::Core::index_t, setStateDyn::payload &) = 0;
+    virtual bool available(setStateDyn const &, Flexus::Core::index_t) = 0;
+    static Flexus::Core::index_t width(configuration &cfg, setStateDyn const &);
+
+    struct getStateDyn
+    {
+        typedef int payload;
+        typedef Flexus::Core::aux_::push port_type;
+        static const bool is_array = true;
+    };
+    static Flexus::Core::index_t width(configuration &cfg, getStateDyn const &);
+
+    struct pullStateInDyn
+    {
+        typedef int payload;
+        typedef Flexus::Core::aux_::pull port_type;
+        static const bool is_array = true;
+    };
+    static Flexus::Core::index_t width(configuration &cfg, pullStateInDyn const &);
+
+    struct pullStateRetDyn
+    {
+        typedef int payload;
+        typedef Flexus::Core::aux_::pull port_type;
+        static const bool is_array = true;
+    };
+    virtual pullStateRetDyn::payload pull(pullStateRetDyn const &, Flexus::Core::index_t) = 0;
+    virtual bool available(pullStateRetDyn const &, Flexus::Core::index_t) = 0;
+    static Flexus::Core::index_t width(configuration &cfg, pullStateRetDyn const &);
+
     struct DummyDrive
     {
         static std::string name()
@@ -124,16 +160,24 @@ struct DummyJumpTable
 {
     typedef DummyInterface iface;
 
-    bool (*wire_available_getState)(Flexus::Core::index_t anIndex);
-    void (*wire_manip_getState)(Flexus::Core::index_t anIndex, iface::getState::payload & aPayload);
+    bool (*wire_available_getState)(Flexus::Core::index_t anIndex) = 0;
+    void (*wire_manip_getState)(Flexus::Core::index_t anIndex, iface::getState::payload & aPayload) = 0;
 
-    bool (*wire_available_pullStateIn)(Flexus::Core::index_t anIndex);
-    void (*wire_manip_pullStateIn)(Flexus::Core::index_t anIndex, iface::pullStateIn::payload & aPayload);
+    bool (*wire_available_pullStateIn)(Flexus::Core::index_t anIndex) = 0;
+    void (*wire_manip_pullStateIn)(Flexus::Core::index_t anIndex, iface::pullStateIn::payload & aPayload) = 0;
+
+    bool (*wire_available_getStateDyn)(Flexus::Core::index_t anIndex) = 0;
+    void (*wire_manip_getStateDyn)(Flexus::Core::index_t anIndex, iface::getStateDyn::payload & aPayload) = 0;
+
+    bool (*wire_available_pullStateInDyn)(Flexus::Core::index_t anIndex) = 0;
+    void (*wire_manip_pullStateInDyn)(Flexus::Core::index_t anIndex, iface::pullStateInDyn::payload & aPayload) = 0;
 
     DummyJumpTable()
     {
         wire_available_getState = 0;
         wire_available_pullStateIn = 0;
+        wire_available_getStateDyn = 0;
+        wire_available_pullStateInDyn = 0;
     }
 
     void check(std::string const &anInstance)
@@ -144,15 +188,7 @@ struct DummyJumpTable
             {
                 if (false)
                 {
-                    if (Flexus::Dbg::Debugger::theDebugger->theMinimumSeverity <= 6 && (1 || !0))
-                    {
-                        if ((true) && true)
-                        {
-                            // Some debug info
-                        }
-                    }
-                    do { }
-                    while (0) ;
+                    // Some debug stuff
                 }
             }
             while (0);
@@ -163,15 +199,29 @@ struct DummyJumpTable
             {
                 if (false)
                 {
-                    if (Flexus::Dbg::Debugger::theDebugger->theMinimumSeverity <= 6 && (1 || !0))
-                    {
-                        if ((true) && true)
-                        {
-                            // Some debug info
-                        }
-                    }
-                    do { }
-                    while (0) ;
+                    // Some debug stuff
+                }
+            }
+            while (0);
+        }
+        if (wire_available_getStateDyn == 0)
+        {
+            do
+            {
+                if (false)
+                {
+                    // Some debug stuff
+                }
+            }
+            while (0);
+        }
+        if (wire_available_pullStateInDyn == 0)
+        {
+            do
+            {
+                if (false)
+                {
+                    // Some debug stuff
                 }
             }
             while (0);
@@ -180,99 +230,144 @@ struct DummyJumpTable
 };
 
 
-#include FLEXUS_END_COMPONENT_DECLARATION()
-#define FLEXUS_END_COMPONENT Dummy
-
-
 namespace nDummy
 {
+class DummyComponent : public Flexus::Core::FlexusComponentBase<DummyComponent, DummyConfiguration, DummyInterface>
+{
+    typedef Flexus::Core::FlexusComponentBase<DummyComponent, DummyConfiguration, DummyInterface> base;
+    typedef base::cfg_t cfg_t;
 
-    class DummyComponent : public Flexus::Core::FlexusComponentBase<DummyComponent, DummyConfiguration, DummyInterface> /**/
+    static std::string componentType()
     {
-        typedef Flexus::Core::FlexusComponentBase<DummyComponent, DummyConfiguration, DummyInterface> base;
-        typedef base::cfg_t cfg_t;
+        return "Dummy";
+    }
 
-        static std::string componentType() { return "Dummy"; }
+public:
+    using base::flexusIndex;
+    using base::flexusWidth;
+    using base::name;
+    using base::statName;
+    using base::cfg;
+    using base::interface;
+    using interface::get_channel;
+    using interface::get_channel_array;
+
+private:
+    typedef base::self self;
+
+public:
+    DummyComponent (cfg_t & aCfg, DummyInterface::jump_table & aJumpTable, Flexus::Core::index_t anIndex, Flexus::Core::index_t aWidth) : base(aCfg, aJumpTable, anIndex, aWidth) {}
+    bool isQuiesced() const
+    {
+        return true;
+    }
     
-    public:
-        using base::flexusIndex;
-        using base::flexusWidth;
-        using base::name;
-        using base::statName;
-        using base::cfg;
-        using base::interface;
-        using interface::get_channel;
-        using interface::get_channel_array;
+    // Initialization
+    void initialize()
+    {
+        std::cout << "Called Init \n";
+        curState = cfg.InitState;
+    }
+    
+    void finalize()
+    {
+        std::cout << "Final state is:" << curState << std::endl;
+    }
+    
+    int getStateNonWire()
+    {
+        return curState;
+    }
+    
+    // setState PushInput Port
+    //=========================
+    bool available(interface::setState const &)
+    {
+        std::cout << "Port:setState is always available\n";
+        return true;
+    }
+    void push(interface::setState const &, int &newState)
+    {
+        std::cout << "Received:" << newState << " on Port:setState\n";
+        curState = newState;
+    }
+    
+    // pullStateRet PullOutput Port
+    // =============================
+    bool available(interface::pullStateRet const &)
+    {
+        return true;
+    } struct eat_semicolon__;
+    int pull(interface::pullStateRet const &)
+    {
+        return curState;
+    }
+    
+    // setStateDyn Dynamic PushInput Port
+    // ===================================
+    bool available(interface::setStateDyn const &, index_t anIndex)
+    {
+        return anIndex % 2 == 0;
+    }
+    void push(interface::setStateDyn const &, index_t anIndex, int &payload)
+    {
+        curState = anIndex * payload;
+    }
 
-    private:
-        typedef base::self self /**/;
+    // pullStateRetDyn Dynamic PullOutput Port
+    // =======================================
+    bool available(interface::pullStateRetDyn const &pn, Flexus::Core::index_t aWidth)
+    {
+        // Some debug stuff
+        return true;
+    } struct eat_semicolon__;
+    int pull(interface::pullStateRetDyn const &, index_t anIndex)
+    {
+        return anIndex * 1000;
+    }
 
-        public:
-        DummyComponent (cfg_t & aCfg, DummyInterface::jump_table & aJumpTable, Flexus::Core::index_t anIndex, Flexus::Core::index_t aWidth) /**/ : base(aCfg, aJumpTable, anIndex, aWidth) {}
-
-        bool isQuiesced() const { return true; }
-
-        // Initialization
-        void initialize()
-        {
-            std::cout << "Called Init \n";
-            curState = cfg.InitState;
-        }
-
-        void finalize()
-        {
-            std::cout << "Final state is:" << curState << std::endl;
-        }
-
-        int getStateNonWire() {
-            return curState;
-        }
-
-        // setState PushInput Port
-        //=========================
-        bool available(interface::setState const &)
-        {
-            std::cout << "Port:setState is always available\n";
-            return true;
-        }
-
-        void push(interface::setState const &, int &newState)
-        {
-            std::cout << "Received:" << newState << " on Port:setState\n";
-            curState = newState;
-        }
-
-        // pullStateRet PullOutput Port
-        // ============================= 
-        bool available(interface::pullStateRet const &)
-        {
-            return true;
-        } struct eat_semicolon__ /**/;
-        int pull(interface::PullStateRet const &)
-        {
-            return curState;
-        }
-
-        // Drive Interfaces
-        void drive(interface::DummyDrive const &)
-        {
-            curState++;
-            std::cout << "Drive Called. Sending incremented state " << curState << " over Port:getState\n";
-            if(get_channel(interface::getState(), jump_table_.wire_available_getState, jump_table_.wire_manip_getState, flexusIndex()).available())
-                get_channel(interface::getState(), jump_table_.wire_available_getState, jump_table_.wire_manip_getState, flexusIndex()) << curState;
-            if(get_channel(interface::pullStateIn(), jump_table_.wire_available_pullStateIn, jump_table_.wire_manip_pullStateIn, flexusIndex()).available())
-                get_channel(interface::pullStateIn(), jump_table_.wire_available_pullStateIn, jump_table_.wire_manip_pullStateIn, flexusIndex()) >> curState;
-        }
-
-
-        private:
-        int curState = 100;
-    };
-
-} // End namespace nDummy
-
+    // Drive Interfaces
+    void drive(interface::DummyDrive const &)
+    {
+        curState++;
+        std::cout << "Drive Called. Sending incremented state " << curState << " over Port:getState\n";
+        if(get_channel(interface::getState(), jump_table_.wire_available_getState, jump_table_.wire_manip_getState, flexusIndex()).available())
+            get_channel(interface::getState(), jump_table_.wire_available_getState, jump_table_.wire_manip_getState, flexusIndex()) << curState;
+        if(get_channel(interface::pullStateIn(), jump_table_.wire_available_pullStateIn, jump_table_.wire_manip_pullStateIn, flexusIndex()).available())
+            get_channel(interface::pullStateIn(), jump_table_.wire_available_pullStateIn, jump_table_.wire_manip_pullStateIn, flexusIndex()) >> curState;
+        int out = 20;
+        if(get_channel_array(interface::getStateDyn(), jump_table_.wire_available_getStateDyn, jump_table_.wire_manip_getStateDyn, flexusIndex(), 3, interface::width(cfg, interface::getStateDyn())).available())
+            get_channel_array(interface::getStateDyn(), jump_table_.wire_available_getStateDyn, jump_table_.wire_manip_getStateDyn, flexusIndex(), 3, interface::width(cfg, interface::getStateDyn())) << out;
+        if(get_channel_array(interface::pullStateInDyn(), jump_table_.wire_available_pullStateInDyn, jump_table_.wire_manip_pullStateInDyn, flexusIndex(), 3, interface::width(cfg, interface::pullStateInDyn())).available())
+            get_channel_array(interface::pullStateInDyn(), jump_table_.wire_available_pullStateInDyn, jump_table_.wire_manip_pullStateInDyn, flexusIndex(), 3, interface::width(cfg, interface::pullStateInDyn())) >> curState;
+    }
+private:
+    int curState = 100;
+};
+}
+// End namespace nDummy
 DummyInterface * DummyInterface::instantiate( DummyConfiguration::cfg_struct_ &aCfg, DummyInterface::jump_table &aJumpTable, Flexus::Core::index_t anIndex, Flexus::Core::index_t aWidth)
 {
     return new nDummy::DummyComponent(aCfg, aJumpTable, anIndex, aWidth);
-} struct eat_semicolon_ /**/;
+}
+struct eat_semicolon_;
 
+Flexus::Core::index_t DummyInterface::width( DummyInterface::configuration &cfg, DummyInterface::setStateDyn const &)
+{
+    return 4;
+}
+Flexus::Core::index_t DummyInterface::width( DummyInterface::configuration &cfg, DummyInterface::getStateDyn const &)
+{
+    return 4;
+}
+Flexus::Core::index_t DummyInterface::width( DummyInterface::configuration &cfg, DummyInterface::pullStateInDyn const &)
+{
+    return 4;
+}
+Flexus::Core::index_t DummyInterface::width( DummyInterface::configuration &cfg, DummyInterface::pullStateRetDyn const &)
+{
+    return 4;
+}
+
+#include FLEXUS_END_COMPONENT_IMPLEMENTATION()
+#define FLEXUS_END_COMPONENT Dummy
