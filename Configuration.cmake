6da1f04b9e39765a461bcc73125d3134f732069e
Bryan Perdrizat <bryan.perdrizat@epfl.ch>
2024-02-21 15:17:20 +0100

    fix: compilation process

--- Configuration.cmake
+++ Configuration.cmake
@@ -47,14 +47,13 @@ set(FLEXUS_ROOT ${CMAKE_SOURCE_DIR})
 # add option -fPIC
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 # add option --std=c++11
-set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
-if(NOT TARGET_PLATFORM)
-  message( STATUS "TARGET_PLATFORM was not specified; using default TARGET_PLATFORM=arm" )
-  set(TARGET_PLATFORM arm)
-endif()
+
+set(TARGET_PLATFORM arm)
+
 
 if(NOT SELECTED_DEBUG)
   message( STATUS "SELECTED_DEBUG was not specified; using default SELECTED_DEBUG=vverb" )
@@ -62,15 +61,18 @@ if(NOT SELECTED_DEBUG)
 endif()
 
 if(NOT SIMULATOR)
-  message( STATUS "SIMULATOR was not specified; using default SIMULATOR=KnottyKraken" )
-  set(SIMULATOR KnottyKraken)
-elseif(SIMULATOR STREQUAL "Harness")
-  message( STATUS "SIMULATOR was defined as Harness. Building testing executable" )
+  message( STATUS "SIMULATOR was not specified; using default SIMULATOR=KeenKraken" )
+  set(SIMULATOR KeenKraken)
 endif()
 
 if(NOT BOOST_INCLUDEDIR)
   message( STATUS "BOOST_INCLUDEDIR was not specified; using default BOOST_INCLUDEDIR=/usr/local/include" )
   set(BOOST_INCLUDEDIR /usr/local/include)
+endif()
+
+if(NOT BOOST_LIBRARYDIR)
+  message( STATUS "BOOST_LIBRARYDIR was not specified; using default BOOST_LIBRARYDIR=/usr/local/lib" )
+  set(BOOST_LIBRARYDIR /usr/local/lib)
 endif()
 
 if(NOT CMAKE_C_COMPILER)
@@ -83,23 +85,19 @@ if(NOT CMAKE_CXX_COMPILER)
   set(CMAKE_CXX_COMPILER /usr/bin/g++)
 endif()
 
-if(NOT BOOST_LIBRARYDIR)
-  message( STATUS "BOOST_LIBRARYDIR was not specified; using default BOOST_LIBRARYDIR=/usr/local/lib" )
-  set(BOOST_LIBRARYDIR /usr/local/lib)
-endif()
 
-if(NOT BUILD_DEBUG)
-    message( STATUS "Building in default optimized configuration" )
-    if(NOT GCC_FLAGS)
-        message( STATUS "GCC_FLAGS was not specified; using default" )
-        set(GCC_FLAGS -O3 -funroll-loops -fno-omit-frame-pointer -g3 -Wall -Werror -fmessage-length=160 -x c++)
-    endif()
-else()
-    message( STATUS "Building in slow debugging configuration (ASAN enabled)" )
-    set(GCC_FLAGS -O1 -g3 -fsanitize=address -Wall -Werror -fmessage-length=160 -x c++)
-    set(GCC_LDFLAGS -fsanitize=address -static-libasan -lrt -ldl)
-    set(LD_PRELOAD $(gcc -print-file-name=libasan.so))
-endif()
+# if(NOT BUILD_DEBUG)
+    # message( STATUS "Building in default optimized configuration" )
+    # if(NOT GCC_FLAGS)
+        # message( STATUS "GCC_FLAGS was not specified; using default" )
+        # set(GCC_FLAGS -O3 -funroll-loops -fno-omit-frame-pointer -g3 -Wall -Werror -fmessage-length=160 -x c++)
+    # endif()
+# else()
+  message( STATUS "Building in slow debugging configuration (ASAN enabled)" )
+  set(GCC_FLAGS -O1 -g3 -fsanitize=address -Wall -Werror -fmessage-length=160 -x c++)
+  set(GCC_LDFLAGS -fsanitize=address -static-libasan -lrt -ldl)
+    # set(LD_PRELOAD $(gcc -print-file-name=libasan.so))
+# endif()
 
 add_definitions(
   -DTARGET_PLATFORM=${TARGET_PLATFORM}
