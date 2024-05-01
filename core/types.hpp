0e3de84937f78b9f75e3ce5343b2f1e5862aed16
Bryan Perdrizat <bryan.perdrizat@epfl.ch>
2024-05-01 16:58:58 +0200

    chore: remove comments block and old 32bits definition

--- core/types.hpp
+++ core/types.hpp
@@ -144,72 +144,9 @@ public:
     ar &address;
   }
 
-}; // class MemoryAddress_
-
-// class dataType : public boost::intrusive_ref_counter<
-//        dataType,
-//        boost::thread_unsafe_counter>
-//{
-// public:
-//    virtual ~dataType(){}
-//    virtual std::size_t size() const  = 0;
-//    template <typename T>
-//        void write(T val);
-
-//};
-
-// template<typename T>
-// class data : public dataType {
-// public:
-//    data() {}
-//    data(T aVal): theVal(aVal) {}
-//    virtual ~data(){}
-//    T theVal;
-//    virtual std::size_t size() const override  {return sizeof(T);}
-
-//};
-
-// template <typename T>
-// void dataType::write(T val){
-//    data<T>* i = dynamic_cast<data<T>*>(this);
-//    i->write(val);
-//}
-
-// typedef boost::intrusive_ptr<dataType> bits;
-
-// enum eDataType{
-//    kuint8_t = 8,
-//    kuint16_t = 16,
-//    kuint32_t = 32,
-//    kuint64_t = 64,
-//    kuint128_t = 128,
-//};
-
-// struct dataFactory{
-//    static bits create(eDataType aDataType){
-//        switch (aDataType) {
-//        case kuint8_t:
-//            return bits(new data<uint8_t>());
-//        case kuint16_t:
-//            return bits(new data<uint16_t>());
-//        case kuint32_t:
-//            return bits(new data<uint32_t>());
-//        case kuint64_t:
-//            return bits(new data<uint64_t>());
-//        case kuint128_t:
-//            return bits(new data<__uint128_t>());
-//        default:
-//            assert(false);
-//            break;
-//        }
-//    }
-
-//};
-
-// typedef boost::dynamic_bitset<> bits;
+};
 typedef boost::multiprecision::uint128_t bits;
 
-// bits fillbits(const int bitSize);
 bool anyBits(bits b);
 bits concat_bits(const bits &lhs, const bits &rhs);
 bits align(uint64_t x, int y);
@@ -226,27 +163,10 @@ namespace SharedTypes {
 
 #define FLEXUS_PhysicalMemoryAddress_TYPE_PROVIDED
 #define FLEXUS_DataWord_TYPE_PROVIDED
-#if (FLEXUS_TARGET_PA_BITS == 32)
-// typedef Core::MemoryAddress_< Core::Word32Bit, false > PhysicalMemoryAddress;
-// typedef Core :: Word32Bit DataWord;
-typedef Core::MemoryAddress_<Core::Word64Bit, false> PhysicalMemoryAddress;
-typedef Core ::Word64Bit DataWord;
-#elif (FLEXUS_TARGET_PA_BITS == 64)
-typedef Core::MemoryAddress_<Core::Word64Bit, false> PhysicalMemoryAddress;
-typedef Core ::Word64Bit DataWord;
-#else
-#error "FLEXUS_TARGET_PA_BITS is not set. Did you forget to set a target?"
-#endif
 
-#define FLEXUS_VirtualMemoryAddress_TYPE_PROVIDED
-#if (FLEXUS_TARGET_VA_BITS == 32)
-// typedef Core::MemoryAddress_< Core::Word32Bit, true > VirtualMemoryAddress;
+typedef Core ::Word64Bit DataWord;
+typedef Core::MemoryAddress_<Core::Word64Bit, false> PhysicalMemoryAddress;
 typedef Core::MemoryAddress_<Core::Word64Bit, true> VirtualMemoryAddress;
-#elif (FLEXUS_TARGET_VA_BITS == 64)
-typedef Core::MemoryAddress_<Core::Word64Bit, true> VirtualMemoryAddress;
-#else
-#error "FLEXUS_TARGET_PA_BITS is not set. Did you forget to set a target?"
-#endif
 
 using Flexus::Core::index_t;
 using Flexus::Core::node_id_t;
