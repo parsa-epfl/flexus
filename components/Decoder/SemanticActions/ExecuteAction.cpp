4597d5acac90a3549133fe825f727df55f54f756
Yuanlong Li <yuanlong.li@epfl.ch>
2025-03-17 11:09:23 +0100

    reserve eu only when necessary

--- components/Decoder/SemanticActions/ExecuteAction.cpp
+++ components/Decoder/SemanticActions/ExecuteAction.cpp
@@ -97,14 +97,14 @@ struct ExecuteAction : public ExecuteBase
         DBG_(VVerb, (<< "Trying to Execute " << *this));
 
         if (ready()) {
-            if (!core()->reqEU(theOperation->euType())) {
-                reschedule();
-                return;
-            }
-
             DBG_(VVerb, (<< "Executing " << *this));
 
             if (theInstruction->hasPredecessorExecuted()) {
+                if (!core()->reqEU(theOperation->euType())) {
+                    reschedule();
+                    return;
+                }
+
                 std::vector<Operand> operands;
                 for (int32_t i = 0; i < numOperands(); ++i) {
                     operands.push_back(op(theOperands[i]));
