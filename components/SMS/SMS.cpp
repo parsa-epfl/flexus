55aa35a04a9933ba939fd01d0bc2b7858fbca648
Ayan Chakraborty <ayan.chakraborty@epfl.ch>
2025-08-10 20:15:00 +0000

    record accesses

--- components/SMS/SMS.cpp
+++ components/SMS/SMS.cpp
@@ -1,4 +1,5 @@
 #include "SMS.hpp"
+using Flexus::SharedTypes::MemoryMessage;
 
 #define FLEXUS_BEGIN_COMPONENT SMS
 #include FLEXUS_BEGIN_COMPONENT_IMPLEMENTATION()
@@ -14,6 +15,7 @@ namespace nuSMS {
 class FLEXUS_COMPONENT(SMS)
 {
     FLEXUS_COMPONENT_IMPL(SMS);
+    uint64_t ts;    // Internal counter for LRU
     PHT thePHT;
     AGT theAGT;
 
@@ -21,16 +23,41 @@ class FLEXUS_COMPONENT(SMS)
         FLEXUS_COMPONENT_CONSTRUCTOR(SMS)
             : base(FLEXUS_PASS_CONSTRUCTOR_ARGS)
         {
+            ts = 0;
+            thePHT = PHT();
+            theAGT = AGT();
         }
 
         FLEXUS_PORT_ALWAYS_AVAILABLE(RequestIn);
-        void push(interface::RequestIn const&, MemoryTransport& aTransport)
+        void push(interface::RequestIn const&, MemoryTransport& aMessage)
         {
-
+            uint64_t addr = aMessage[MemoryMessageTag]->address();  // TODO: use flexus datatypes
+            uint64_t pc = aMessage[MemoryMessageTag]->pc();
+            bool is_store;
+            switch (aMessage[MemoryMessageTag]->type()) {
+                case MemoryMessage::MemoryMessageType::LoadReq:
+                case MemoryMessage::MemoryMessageType::ReadReq:
+                    is_store = false;
+                    break;
+                case MemoryMessage::MemoryMessageType::StoreReq:
+                case MemoryMessage::MemoryMessageType::RMWReq:
+                case MemoryMessage::MemoryMessageType::CmpxReq:
+                case MemoryMessage::MemoryMessageType::WriteReq:
+                case MemoryMessage::MemoryMessageType::WriteAllocate:
+                    is_store = true;
+                    break;
+                default:
+                    return;
+            }
+            auto entry = theAGT.record(addr, pc, is_store, ts);
+            if (entry) {
+                thePHT.insert(*entry);
+            }
         }
 
         void drive(interface::SMSDrive const&) override
         {
+            ts++;
 
         }
 
